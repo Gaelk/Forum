c77a6851185b44493aa91ec11b09befaec763a6b
formation <icebrg07@live.fr>
2017-09-04 11:58:27 +0200

    creation d'un formulaire:   *config.yml: # Twig Configuration   -config du css formulaire 'form_themes' pour les Twig avec     bootstrap_3
      *AdminController.php:
       -creation d'un objet theme pour les formulaires
       -recuperation des inputs
       -persistance

--- src/AppBundle/Controller/AdminController.php
+++ src/AppBundle/Controller/AdminController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Theme;
+use AppBundle\Form\ThemeType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -19,13 +21,32 @@ class AdminController extends Controller
      * @Route("/themes", name="admin_themes")
      * @return Response
      */
-    public function themeAction(){
+    public function themeAction(Request $request){
+
+
         $repository = $this->getDoctrine()
             ->getRepository("AppBundle:Theme");
-
         $themeList = $repository->findAll();
 
-        return $this->render("admin/theme.html.twig", ["themeList" => $themeList]);
+
+        //creation du formulaire
+        $theme= new Theme();
+        $form=$this->createForm(ThemeType::class, $theme);
+
+        //hydratation de l'entité   | $_GET est recuperé avec une variable  $request de type Request en param de la function
+        $form->handleRequest($request);
+
+        //traitement du formulaire
+
+        if ($form->isSubmitted() and $form->isValid()){
+            //Persistance de l'entité
+            $em=$this->getDoctrine()->getManager();
+            $em->persist($theme);
+            $em->flush();
+
+            return $this->redirectToRoute("admin/themes");
+        }
+        return $this->render("admin/theme.html.twig", ["themeList" => $themeList, "themeForm"=>$form->createView()]);
     }
 
 }
